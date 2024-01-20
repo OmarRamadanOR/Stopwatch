69580253722ae62de2cfb3feace7786e65372ef2
OmarRamadanOR <155376276+OmarRamadanOR@users.noreply.github.com>
2024-01-20 11:21:41 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+const displayTime = document.querySelector(".display-time");
+
+let [seconds, minutes, hours] = [0, 0, 0];
+let timer = null;
+
+// Main Function
+function stopWatch() {
+    seconds++;
+    if (seconds === 60) {
+        seconds = 0;
+        minutes++;
+    } else if (minutes === 60) {
+        minutes = 0;
+        hours++;
+    }
+
+    let h = hours < 10 ? `0${hours}` : hours;
+    let m = minutes < 10 ? `0${minutes}` : minutes;
+    let s = seconds < 10 ? `0${seconds}` : seconds;
+
+    displayTime.innerHTML=`${h} : ${m} : ${s}`
+}
+
+// Start 
+function watchStart() {
+    if (timer !== null) {
+        clearInterval(timer)
+    }
+    timer = setInterval(stopWatch, 1000);
+}
+
+// Stop
+function watchStop() {
+    clearInterval(timer)
+}
+
+//Reset
+function watchReset() {
+    clearInterval(timer)
+    seconds = 0;
+    minutes = 0;
+    hours   = 0;
+    displayTime.innerHTML = `00:00:00`;
+}
